e8651846b3d5f9e70cecdf136e3cf714f0f61f05
CyrusWarner <cyruswarner27@gmail.com>
2021-09-20 18:35:55 -0500

    Added functionality for clicking next question button.

--- src/App.tsx
+++ src/App.tsx
@@ -59,7 +59,18 @@ const App = () => {
     }
   };
 
-  const nextQuestion = () => {};
+  const nextQuestion = () => {
+    // Move on to the next question if not the last question
+    const nextQuestion = number + 1
+
+    if(nextQuestion === TOTAL_QUESTIONS){
+      setGameOver(true);
+      
+    }
+    else{
+      setNumber(nextQuestion);
+    }
+  };
   return (
     <div className="App">
       <h1>React Quiz</h1>
