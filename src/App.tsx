6dcc9be096a46617e656ce4db1bb26ac72ab412b
CyrusWarner <cyruswarner27@gmail.com>
2021-09-20 17:13:03 -0500

    Added initial state values and passed props to question card.

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,42 @@
-import React, { MouseEvent } from 'react';
+import React, { MouseEvent, useState } from "react";
 //Components
-import QuestionCard from './components/QuestionCard';
+import QuestionCard from "./components/QuestionCard";
+
+const TOTAL_QUESTIONS = 10;
+
 const App = () => {
-  const startQuiz = async () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+  const startQuiz = async () => {};
 
-  }
+  const checkAnswer = (e: MouseEvent) => {};
 
-  const checkAnswer = (e: MouseEvent) => {
-    
-  }
-
-  const nextQuestion = () => {
-
-  }
+  const nextQuestion = () => {};
   return (
     <div className="App">
       <h1>React Quiz</h1>
-      <button className="start" onClick={startQuiz}>Start Quiz</button>
+      <button className="start" onClick={startQuiz}>
+        Start Quiz
+      </button>
       <p className="score">Score:</p>
       <p>Loading Questions ...</p>
-      {/* <QuestionCard /> */}
+      <QuestionCard
+        questionNumber={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      />
       <button className="next" onClick={nextQuestion}>
         Next Question
       </button>
     </div>
   );
-}
+};
 
 export default App;
