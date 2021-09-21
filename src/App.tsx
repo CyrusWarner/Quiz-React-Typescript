c15c32ca3df88dd2afd87ea8f36844dd6ddec32d
CyrusWarner <cyruswarner27@gmail.com>
2021-09-21 10:07:47 -0500

    Added functionality for selecting a difficulty.

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ const App = () => {
   const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
+  const [difficulty, setDifficulty] = useState<Difficulty>(Difficulty.EASY)
 
   const startQuiz = async () => {
     setLoading(true);
@@ -30,7 +31,7 @@ const App = () => {
 
     const newQuestions = await fetchQuizQuestions(
       TOTAL_QUESTIONS,
-      Difficulty.EASY
+      difficulty
     );
 
     setQuestions(newQuestions);
@@ -41,13 +42,13 @@ const App = () => {
   };
 
   const checkAnswer = (e: MouseEvent<HTMLButtonElement>) => {
-    if(!gameOver) {
+    if (!gameOver) {
       // Users answer
       const answer: string = e.currentTarget.value;
       // Check answer against the correct answer
       const correct: boolean = questions[number].correct_answer === answer;
       // Add score if answer is correct
-      if(correct) setScore(previous => previous + 1);
+      if (correct) setScore((previous) => previous + 1);
       //Save answer in the array for users answers
       // answer, is the same as answer: answer
       const answerObject = {
@@ -62,44 +63,56 @@ const App = () => {
 
   const nextQuestion = () => {
     // Move on to the next question if not the last question
-    const nextQuestion = number + 1
+    const nextQuestion = number + 1;
 
-    if(nextQuestion === TOTAL_QUESTIONS){
+    if (nextQuestion === TOTAL_QUESTIONS) {
       setGameOver(true);
-      
-    }
-    else{
+    } else {
       setNumber(nextQuestion);
     }
   };
+
+  const checkDifficulty = (e: MouseEvent<HTMLButtonElement>) => {
+    // Checks the sellected difficulty
+    let difficulty: string = e.currentTarget.value;
+    if(difficulty === Difficulty.EASY) setDifficulty(Difficulty.EASY);
+    if(difficulty === Difficulty.MEDIUM) setDifficulty(Difficulty.MEDIUM);
+    if(difficulty === Difficulty.HARD) setDifficulty(Difficulty.HARD);
+  }
   return (
     <>
-    <GlobalStyle />
-    <Wrapper>
-      <h1>React Quiz</h1>
-      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
-        <button className="start" onClick={startQuiz}>
-          Start Quiz
-        </button>
-      ) : null}
-      {!gameOver ? <p className="score">Score: {score}</p> : null}
-      {loading && <p>Loading Questions ...</p>}
-      {!loading && !gameOver &&
-      <QuestionCard
-        questionNumber={number + 1}
-        totalQuestions={TOTAL_QUESTIONS}
-        question={questions[number].question}
-        answers={questions[number].answers}
-        userAnswer={userAnswers ? userAnswers[number] : undefined}
-        callback={checkAnswer}
-      />
-      }
-      {!gameOver && !loading && userAnswers.length === number + 1 && number !== TOTAL_QUESTIONS - 1 &&
-      <button className="next" onClick={nextQuestion}>
-        Next Question
-      </button>
-      }
-    </Wrapper>
+      <GlobalStyle />
+      <Wrapper>
+        <h1>React Quiz</h1>
+        {(gameOver || (userAnswers.length === TOTAL_QUESTIONS)) && difficulty ? (
+          <button className="start" onClick={startQuiz}>
+            Start Quiz
+          </button>
+        ) : null}
+        {!gameOver ? <p className="score">Score: {score}</p> : null}
+        {loading && <p>Loading Questions ...</p>}
+        <button onClick={checkDifficulty} value={Difficulty.EASY} className="start">Easy</button>
+        <button onClick={checkDifficulty} value={Difficulty.MEDIUM} className="start">Medium</button>
+        <button onClick={checkDifficulty} value={Difficulty.HARD} className="start">Hard</button>
+        {!loading && !gameOver && (
+          <QuestionCard
+            questionNumber={number + 1}
+            totalQuestions={TOTAL_QUESTIONS}
+            question={questions[number].question}
+            answers={questions[number].answers}
+            userAnswer={userAnswers ? userAnswers[number] : undefined}
+            callback={checkAnswer}
+          />
+        )}
+        {!gameOver &&
+          !loading &&
+          userAnswers.length === number + 1 &&
+          number !== TOTAL_QUESTIONS - 1 && (
+            <button className="next" onClick={nextQuestion}>
+              Next Question
+            </button>
+          )}
+      </Wrapper>
     </>
   );
 };
